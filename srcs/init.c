4675d9e6cc8e955ff81228432ff14d3f7b042c68
Avdu <avduli.avdul@gmail.com>
2024-09-02 14:33:22 +0200

    go

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:24:44 by albertini         #+#    #+#             */
-/*   Updated: 2024/08/28 13:16:22 by aavduli          ###   ########.fr       */
+/*   Updated: 2024/09/02 14:28:42 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	init_mlx(t_game *game)
 {
 	game->mlx = mlx_init();
-	game->win = mlx_new_window(game->mlx, game->width, game->height, "Cube3D");
+	game->win = mlx_new_window(game->mlx, 1080, 720, "Cube3D");
 }
 
 void	init_game(char *av, t_game *game)
