70ebb7675b5a6d1d68aead2f762246e0edb59971
Avdu <avduli.avdul@gmail.com>
2024-10-02 15:03:03 +0200

    dubble patch

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,22 +6,11 @@
 /*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 12:53:21 by albertini         #+#    #+#             */
-/*   Updated: 2024/10/02 14:55:07 by aavduli          ###   ########.fr       */
+/*   Updated: 2024/10/02 14:59:55 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
-
-void	data_init_bis(t_game *game)
-{
-	game->paths.no_path = NULL;
-	game->paths.so_path = NULL;
-	game->paths.we_path = NULL;
-	game->paths.ea_path = NULL;
-	game->map = NULL;
-	game->paths.c_color = 0;
-	game->paths.f_color = 0;
-}
 
 static inline int	av_checker(char *str)
 {
---
+++ srcs/utils.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 14:59:09 by aavduli           #+#    #+#             */
+/*   Updated: 2024/10/02 14:59:36 by aavduli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+void	data_init_bis(t_game *game)
+{
+	game->paths.no_path = NULL;
+	game->paths.so_path = NULL;
+	game->paths.we_path = NULL;
+	game->paths.ea_path = NULL;
+	game->map = NULL;
+	game->paths.c_color = 0;
+	game->paths.f_color = 0;
+}
+
