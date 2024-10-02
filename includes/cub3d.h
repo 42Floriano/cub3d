44804113bd607496d9e76a047dc2c7e1bfe253d6
Floriano Albertini <floriano.webdev@gmail.com>
2024-10-02 16:39:54 +0200

    Backup

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
+/*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 17:46:15 by aavduli           #+#    #+#             */
-/*   Updated: 2024/10/02 16:04:34 by aavduli          ###   ########.fr       */
+/*   Updated: 2024/10/02 16:21:04 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@
 # include <stdbool.h>
 # include <sys/time.h>
 
-# define MAP_WIDTH 6
-# define MAP_HEIGHT 5
+# define MAP_WIDTH 29
+# define MAP_HEIGHT 13
 # define SCREEN_WIDTH 1280
 # define SCREEN_HEIGHT 1080
 # define FLOOR_COLOR 0x00FF00
