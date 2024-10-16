4a3f4598713e592bce48cb2f31884dfdcec1c7ab
Floriano Albertini <floriano.webdev@gmail.com>
2024-10-16 16:05:44 +0200

    backup

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 17:46:15 by aavduli           #+#    #+#             */
-/*   Updated: 2024/10/16 15:39:43 by falberti         ###   ########.fr       */
+/*   Updated: 2024/10/16 15:58:48 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,8 +52,8 @@
 # define RIGHT			1
 # define LEFT			0
 
-# define MOVE_SPEED 	0.05
-# define R_SPD 			0.07
+# define MOVE_SPEED 	0.04
+# define R_SPD 			0.08
 
 /* ERROR MESSAGES */
 # define ERR_ARGS		"Invalid number of arguments"
