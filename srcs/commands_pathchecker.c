d190dc4c21a34de4b946b4d8ce4e974a73d0c09b
Avdu <avduli.avdul@gmail.com>
2024-10-22 15:14:05 +0200

    deleted modif for test

--- srcs/commands_pathchecker.c
+++ srcs/commands_pathchecker.c
@@ -6,7 +6,7 @@
 /*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 14:13:28 by albertini         #+#    #+#             */
-/*   Updated: 2024/10/22 15:06:14 by aavduli          ###   ########.fr       */
+/*   Updated: 2024/10/22 15:13:41 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,25 +28,15 @@ static bool	check_path_for_walls(t_game *game, t_movement *move, int num_steps)
 	check_x = game->posx;
 	check_y = game->posy;
 	i = 0;
-
 	while (i < num_steps)
 	{
 		check_x += move->step_x;
 		check_y += move->step_y;
-
-		// Vérification que check_x et check_y ne sortent pas des limites de la carte
-		if ((int)check_x < 0 || (int)check_x >= game->map_width ||
-			(int)check_y < 0 || (int)check_y >= game->map_height)
-			return (false);  // On considère le bord de la carte comme un mur
-
-		// Vérification de la présence d'un obstacle
 		if (game->map[(int)check_x][(int)check_y] != '0')
 			return (false);
-		
 		i++;
 	}
-
-	return (true);  // Aucun obstacle trouvé
+	return (true);
 }
 
 bool	is_path_clear(t_game *game, double end_x, double end_y)
