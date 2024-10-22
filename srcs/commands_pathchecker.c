c53443f839deb05fc088c02b692c38272fd7f503
Avdu <avduli.avdul@gmail.com>
2024-10-22 15:12:39 +0200

    can't found the bug

--- srcs/commands_pathchecker.c
+++ srcs/commands_pathchecker.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   commands_pathchecker.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: albertini <albertini@student.42.fr>        +#+  +:+       +#+        */
+/*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 14:13:28 by albertini         #+#    #+#             */
-/*   Updated: 2024/10/11 14:55:06 by albertini        ###   ########.fr       */
+/*   Updated: 2024/10/22 15:06:14 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,25 @@ static bool	check_path_for_walls(t_game *game, t_movement *move, int num_steps)
 	check_x = game->posx;
 	check_y = game->posy;
 	i = 0;
+
 	while (i < num_steps)
 	{
 		check_x += move->step_x;
 		check_y += move->step_y;
+
+		// Vérification que check_x et check_y ne sortent pas des limites de la carte
+		if ((int)check_x < 0 || (int)check_x >= game->map_width ||
+			(int)check_y < 0 || (int)check_y >= game->map_height)
+			return (false);  // On considère le bord de la carte comme un mur
+
+		// Vérification de la présence d'un obstacle
 		if (game->map[(int)check_x][(int)check_y] != '0')
 			return (false);
+		
 		i++;
 	}
-	return (true);
+
+	return (true);  // Aucun obstacle trouvé
 }
 
 bool	is_path_clear(t_game *game, double end_x, double end_y)
