600dc88572023e17760c3ba95ee7453006f8259f
Avdu <avduli.avdul@gmail.com>
2024-10-22 15:17:08 +0200

    stupid

--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,66 +6,52 @@
 /*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 12:04:48 by aavduli           #+#    #+#             */
-/*   Updated: 2024/10/22 14:56:17 by aavduli          ###   ########.fr       */
+/*   Updated: 2024/10/22 15:15:46 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-#define MINIMAP_SCALE 20  // Chaque carré de la carte sera réduit par un facteur de 10
+#define MINIMAP_SCALE 20
 
 void	render_minimap(t_game *game)
 {
-	int map_x;
-	int map_y;
-	int pixel_x;
-	int pixel_y;
-	int color;
+	int	map_x;
+	int	map_y;
+	int	pixel_x;
+	int	pixel_y;
+	int	color;
 
-	// Initialisation de la boucle pour parcourir les lignes de la carte
 	map_y = 0;
 	while (map_y < game->map_height)
 	{
-		// Initialisation de la boucle pour parcourir les colonnes de la carte
 		map_x = 0;
 		while (map_x < game->map_width)
 		{
-			// Choisir la couleur selon le type de case
-			if (game->map[map_y][map_x] == '1')  // Mur
-				color = 0xFFFFFF;  // Blanc pour les murs
-			else if (game->map[map_y][map_x] == '0')  // Vide
-				color = 0x000000;  // Noir pour les espaces vides
+			if (game->map[map_y][map_x] == '1')
+				color = 0xFFFFFF;
+			else if (game->map[map_y][map_x] == '0')
+				color = 0x000000;
 			else
-				color = 0x00FF00;  // Autre (portes, objets, etc.)
-
-			// Dessiner un carré pour chaque case de la carte
+				color = 0x00FF00;
 			pixel_y = 0;
 			while (pixel_y < MINIMAP_SCALE)
 			{
 				pixel_x = 0;
 				while (pixel_x < MINIMAP_SCALE)
 				{
-					// Inverser l'axe Y pour correspondre à la logique du jeu
 					my_mlx_pixel_put(game, map_x * MINIMAP_SCALE + pixel_x,
 						(game->map_height - 1 - map_y) * MINIMAP_SCALE + pixel_y, color);
 					pixel_x++;
 				}
 				pixel_y++;
 			}
-
-			// Passer à la prochaine case de la carte
 			map_x++;
 		}
-
-		// Passer à la prochaine ligne de la carte
 		map_y++;
 	}
-
-	// Dessiner la position du joueur sur la mini-carte
 	int player_map_x = (int)(game->posx * MINIMAP_SCALE);
 	int player_map_y = (int)((game->map_height - 1 - game->posy) * MINIMAP_SCALE);
-
-	// Dessiner le joueur comme un petit carré rouge
 	pixel_y = -2;
 	while (pixel_y <= 2)
 	{
