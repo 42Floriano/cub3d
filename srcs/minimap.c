8a73bc8db159a80573011d8c7836210ae7ffa173
Avdu <avduli.avdul@gmail.com>
2024-10-23 11:08:40 +0200

    inversed map

--- srcs/minimap.c
+++ srcs/minimap.c
@@ -6,7 +6,7 @@
 /*   By: aavduli <aavduli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 12:04:48 by aavduli           #+#    #+#             */
-/*   Updated: 2024/10/22 15:17:38 by aavduli          ###   ########.fr       */
+/*   Updated: 2024/10/23 11:08:23 by aavduli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,21 @@ void	render_minimap(t_game *game)
 		while (map_x < game->map_width)
 		{
 			if (game->map[map_y][map_x] == '1')
-				color = 0xFFFFFF;
+				color = 0xFFFFFF;  // Blanc pour les murs
 			else if (game->map[map_y][map_x] == '0')
-				color = 0x000000;
+				color = 0x000000;  // Noir pour les espaces vides
 			else
-				color = 0x00FF00;
+				color = 0x00FF00;  // Autre (portes, objets, etc.)
+
 			pixel_y = 0;
 			while (pixel_y < MINIMAP_SCALE)
 			{
 				pixel_x = 0;
 				while (pixel_x < MINIMAP_SCALE)
 				{
+					// Correction de l'inversion de l'axe Y
 					my_mlx_pixel_put(game, map_x * MINIMAP_SCALE + pixel_x,
-						(game->map_height - 1 - map_y) * MINIMAP_SCALE + pixel_y, color);
+						map_y * MINIMAP_SCALE + pixel_y, color);
 					pixel_x++;
 				}
 				pixel_y++;
@@ -50,15 +52,19 @@ void	render_minimap(t_game *game)
 		}
 		map_y++;
 	}
+
+	// Dessiner la position du joueur sur la mini-carte
 	int player_map_x = (int)(game->posx * MINIMAP_SCALE);
-	int player_map_y = (int)((game->map_height - 1 - game->posy) * MINIMAP_SCALE);
+	int player_map_y = (int)(game->posy * MINIMAP_SCALE);
+
+	// Dessiner le joueur comme un petit carré rouge
 	pixel_y = -2;
 	while (pixel_y <= 2)
 	{
 		pixel_x = -2;
 		while (pixel_x <= 2)
 		{
-			my_mlx_pixel_put(game, player_map_x + pixel_x, player_map_y + pixel_y, 0xFF0000);
+			my_mlx_pixel_put(game, player_map_x + pixel_x, player_map_y + pixel_y, 0xFF0000);  // Rouge
 			pixel_x++;
 		}
 		pixel_y++;
